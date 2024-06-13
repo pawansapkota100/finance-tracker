c6aa54067207a272fd198d3ff9175fe245b4155d
pawan sapkota <pawansapkota@pawans-MacBook-Air.local>
2024-06-13 08:41:20 +0545

    fix: vercel config

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 sudo apt-get install python3-pip
 # Check if pip is installed
-if ! command -v pip &> /dev/null
-then
-    echo "Error: pip is not installed. Please install pip."
-      # For Python 3
-    exit 1
-fi
+
 
 # Install Python dependencies
 pip install -r requirements.txt
