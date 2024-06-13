585923e530760de723c4d209d1c676fe5f48e712
pawan sapkota <pawansapkota@pawans-MacBook-Air.local>
2024-06-13 08:37:38 +0545

    fix: vercel config

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+# Check if pip is installed
+if ! command -v pip &> /dev/null
+then
+    echo "Error: pip is not installed. Please install pip."
+    exit 1
+fi
+
+# Install Python dependencies
 pip install -r requirements.txt
-3.11.9 manage.py collectstatic --noinput --clear
+
+# Run Django collectstatic command
+python3 manage.py collectstatic --noinput --clear
